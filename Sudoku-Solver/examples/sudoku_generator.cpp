680dc3f8a26f0291e752deacd630db7855392a40
devadlakha711 <165180934+devadlakha711@users.noreply.github.com>
2024-06-21 16:38:01 +0530

    Add files via upload

---
+++ Sudoku-Solver/examples/sudoku_generator.cpp
@@ -0,0 +1,12 @@
+/* Copyright 2024 DEV ADLAKHA */
+/* A basic example as seen in the project's README. */
+
+#include<iostream>
+#include"../src/sudoku_generator.h"
+
+int main() {
+    sudoku::Grid grid = sudoku::generate_puzzle();
+    std::cout << grid << std::endl;
+
+    return 0;
+}
---
+++ Sudoku-Solver/src/coord.h
@@ -0,0 +1,14 @@
+/* Copyright 2024 DEV ADLAKHA */
+#ifndef SRC_COORD_H_
+#define SRC_COORD_H_
+
+#include<utility>
+
+namespace sudoku {
+
+typedef std::pair<int, int> Coord;
+int GRID_LEN = 9;
+
+}  // namespace sudoku
+
+#endif  // SRC_COORD_H_
